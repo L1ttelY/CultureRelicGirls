df03696ff89ea828cc97c27d3e79a48d5163d766
L1ttel_Y <L1ttel_Y@126.com>
2022-06-23 15:13:43 +0800

    完成了角色的移动

--- Assets/Museum/UI/UIModeBase.cs
+++ Assets/Museum/UI/UIModeBase.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Museum {
+
+	/// <summary>
+	/// UI界面的基类
+	/// 要创建新的UI界面请继承此类
+	/// 用重载的Init来做初始化函数 而不是Start
+	/// </summary>
 	public class UIModeBase:MonoBehaviour {
 
 		public void Init() {
