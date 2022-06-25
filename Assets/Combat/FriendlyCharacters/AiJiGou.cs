d68b8110ce2e17f3ed0a38b848c4991827c19c9b
曾经 <2079447256@qq.com>
2022-06-26 00:12:47 +0800

    人物技能完成

---
+++ Assets/Combat/FriendlyCharacters/AiJiGou.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Combat
+{
+
+	public class AiJiGou : EntityFriendly
+	{
+		
+		DamageModel aoe;
+		public int aoeDamage;
+		float times = 2;
+
+		protected override void Start()
+		{
+			base.Start();
+			EntityBase.UpdateStats += EntityBase_UpdateStats;
+			EntityBase.DamageEvent += EntityBase_DamageEvent;
+			aoe = GetDamage();
+			aoe.amount = aoeDamage;
+			aoe.knockback = 0;
+		}
+
+		private void EntityBase_DamageEvent(object sender, DamageModel e)
+		{
+			if (times > attackCd)
+			{
+				times = 0;
+				foreach (var i in entities)
+				{
+					if (!(i is EntityFriendly))
+					{
+						if (i.transform.position.x - this.transform.position.x < 12 )
+						{
+							i.Damage(aoe);
+						}
+					}
+				}
+			}
+	    }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+			times += Time.deltaTime;
+        }
+
+        protected override void OnDestroy()
+		{
+			base.OnDestroy();
+			EntityBase.UpdateStats -= EntityBase_UpdateStats;
+			EntityBase.DamageEvent -= EntityBase_DamageEvent;
+
+		}
+		private void EntityBase_UpdateStats(object _sender)
+		{
+			//判断是否响应
+			EntityBase sender = _sender as EntityBase;
+			if (sender != this) return;
+
+			//准备相应
+			//注意用+= 不要用*=或=
+			Debug.Log(this.tag);
+		}
+	}
+
+}
