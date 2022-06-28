c4f5bc12effbf8daa019fbdb99cedafbb81cf5d4
曾经 <2079447256@qq.com>
2022-06-28 23:28:38 +0800

    森林关卡

--- Assets/Combat/Fighter.cs
+++ Assets/Combat/Fighter.cs
@@ -6,10 +6,32 @@ namespace Combat {
 
 	public class Fighter:EntityEnemy {
 		[SerializeField] float knockbackResistance=1;
-		protected override void StartKnockback(float knockback,int direction) {
-			base.StartKnockback(knockback*(1-knockbackResistance),direction);
-		}
+        float CurrentKnockbackResistance = 0 ; //此时刻的击退抗性
+        public Sprite BlockDefine; //得到block的精灵，用于鉴别block
 
-	}
+        protected override void StartKnockback(float knockback,int direction) {
+			base.StartKnockback(knockback*(1-CurrentKnockbackResistance),direction);//这里的变量换了下
+        }
+        protected override void Update()
+        {
+            base.Update();
+           
+            CurrentKnockbackResistance = knockbackResistance;
+           
+            foreach (var i in entities)
+            {
+                //判断前方有没有盾
+                if (i is EntityEnemy)
+                {
+                    float distance = this.transform.position.x - i.transform.position.x;
+                    Sprite whoAmI = i.GetComponentInChildren<SpriteRenderer>().sprite;
+                    if (whoAmI == BlockDefine && distance < 4) //前方是盾，距离小
+                        CurrentKnockbackResistance = 0.9f;
+                }
+
+            }
+        }
+
+    }
 
 }
