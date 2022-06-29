4d21cc4c2dbcb244d31f2953115ee9c44c9a31a6
L1ttel_Y <L1ttel_Y@126.com>
2022-06-30 00:18:28 +0800

    修复了维修娘移动的bug 为按钮增加了音效

---
+++ Assets/Audio/ButtonSound.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound:MonoBehaviour {
+
+	[SerializeField] AudioClip audioClip;
+
+	private void Start() {
+		Button[] buttons = Resources.FindObjectsOfTypeAll<Button>();
+
+		foreach(var i in buttons) {
+			Debug.Log(i.name);
+			i.onClick.AddListener(PlaySound);
+		}
+		instance=this;
+	}
+
+	public static ButtonSound instance { get; private set; }
+
+	public void PlaySound() {
+		AudioController.PlayAudio(audioClip,Camera.main.transform.position);
+	}
+
+}
