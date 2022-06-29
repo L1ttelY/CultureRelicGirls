5c9cfe0e7325d9dedd5ac12d7c33b7afd5ab2c21
L1ttel_Y <L1ttel_Y@126.com>
2022-06-29 22:18:20 +0800

    完成了全物品存档(StreamingAssets/满级存档.xml) 在博物馆中安置了角色

--- Assets/Characters/CharacterData.cs
+++ Assets/Characters/CharacterData.cs
@@ -15,9 +15,9 @@ public class CharacterData:ScriptableObject {
 		datas.Add(id,this);
 	}
 
-	[field: TextArea] public string descriptionShort { get; private set; }
-	[field: TextArea] public string skillDescription { get; private set; }
-	[field: TextArea] public string fullDescription { get; private set; }
+	[field: TextArea] [field: SerializeField] public string descriptionShort { get; private set; } //在选择人物后升级界面所展示的介绍
+	[field: TextArea] [field: SerializeField] public string skillDescription { get; private set; } //角色的技能介绍
+	[field: TextArea] [field: SerializeField] public string fullDescription { get; private set; }  //角色的背景介绍
 
 	[field: SerializeField] public int id { get; private set; }
 	[field: SerializeField] public int maxLevel { get; private set; }
