fac8ebb946bf81b553747b1f4faaeb7c6b0c72fc
L1ttel_Y <L1ttel_Y@126.com>
2023-03-04 15:03:08 +0800

    完成了可交互场景物体组件 切换场景似乎有一些bug

--- Assets/Combat/Scenes/Interactable/Interactable.cs
+++ Assets/Combat/Scenes/Interactable/Interactable.cs
@@ -5,6 +5,51 @@ using UnityEngine;
 namespace Combat {
 
 	public class Interactable:MonoBehaviour {
+
+		static HashSet<Interactable> instances = new HashSet<Interactable>();
+		public static bool CanInteract() {
+			foreach(var i in instances) {
+				if(!i.enabled) continue;
+				if(i.distanceToCharacter<0.75f) return true;
+			}
+			return false;
+		}
+		public static void DoInteract() {
+			if(!CanInteract()) return;
+			float minDistance = 1;
+			Interactable target = null;
+			foreach(var i in instances) {
+				if(!i.enabled) continue;
+				if(i.distanceToCharacter<minDistance) {
+					minDistance=i.distanceToCharacter;
+					target=i;
+				}
+			}
+
+			if(target==null) return;
+			target.onInteract.Invoke();
+
+		}
+
+		[SerializeField] UnityEngine.Events.UnityEvent onInteract;
+		private void Update() {
+			distanceToCharacter=1;
+			for(int i = 0;i<3;i++) {
+				if(!EntityFriendly.friendlyList[i]) continue;
+				float newDistance = Mathf.Abs(EntityFriendly.friendlyList[i].transform.position.x-transform.position.x);
+				if(newDistance<distanceToCharacter) distanceToCharacter=newDistance;
+			}
+		}
+
+		float distanceToCharacter;
+
+		private void Start() {
+			instances.Add(this);
+		}
+		private void OnDestroy() {
+			instances.Remove(this);
+		}
+
 	}
 
 }
