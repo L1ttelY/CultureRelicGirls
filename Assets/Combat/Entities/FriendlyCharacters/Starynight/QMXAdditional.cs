9eba22798a1a72d18b650f629e0109bd53f1f816
Lsghostkiller <Lsghostkiller@gmail.com>
2023-06-02 00:20:17 +0800

    星空实装

---
+++ Assets/Combat/Entities/FriendlyCharacters/Starynight/QMXAdditional.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Combat {
+
+	public class QMXAdditional:EntityAdditionalFunctionBase {
+
+		[SerializeField] int projectileCount;
+		[SerializeField] float rangeMin;
+		[SerializeField] float rangeMax;
+
+		static List<EntityBase> targets = new List<EntityBase>();
+
+		public override bool OverrideAttack(EntityBase target,int attack) {
+
+			if(attack==-1) {
+
+				targets.Clear();
+				foreach(var i in EntityBase.entities) {
+					if(i.isFriendly) continue;
+					if(!i.isActiveAndEnabled) continue;
+					if(Mathf.Abs(i.transform.position.x-transform.position.x)<rangeMin) continue;
+					if(Mathf.Abs(i.transform.position.x-transform.position.x)>rangeMax) continue;
+					targets.Add(i);
+				}
+				if(targets.Count==0) return true;
+				for(int i=0;i<projectileCount;i++){
+					int targetIndex = Random.Range(0,targets.Count);
+					entity.Attack(targets[targetIndex],0);
+				}
+
+				return true;
+			}
+
+			return false;
+
+		}
+
+	}
+
+}
