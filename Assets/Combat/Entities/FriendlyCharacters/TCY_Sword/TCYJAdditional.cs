cd8808c5f3d135b24d10ce8b82d53b1a7b180e35
Lsghostkiller <Lsghostkiller@gmail.com>
2023-06-08 21:46:40 +0800

    天从云剑实装

---
+++ Assets/Combat/Entities/FriendlyCharacters/TCY_Sword/TCYJAdditional.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Combat{
+
+  public class TCYJAdditional : EntityAdditionalFunctionBase{
+
+    [SerializeReference] GameObject objectToSummon;
+
+		public override bool OverrideAttack(EntityBase target,int attack) {
+
+			if(attack==-1){
+				Instantiate(objectToSummon,transform.position,Quaternion.identity,transform.parent);
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+
+}
